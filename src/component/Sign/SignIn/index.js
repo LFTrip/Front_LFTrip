09963635e4c9a10393b49a58fb59bec4788be7ed
saneto <aymanezaircha@gmail.com>
2020-02-21 02:10:52 +0100

    gestion des erreurs

--- src/component/Sign/SignIn/index.js
+++ src/component/Sign/SignIn/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
 import { SignUpLink } from '../SignUp';
 import { withBackaccessContext } from '../../BackEnd';
 import * as ROUTES from '../../../const/routes';
 import { PasswordForgetLink } from '../../Password/PasswordForget';
+import { FormErrors } from '../../Error/index';
+
 
 	const SignInPage = () => (
 		<section id="login-page">
@@ -16,7 +19,11 @@ import { PasswordForgetLink } from '../../Password/PasswordForget';
 const INITIAL_STATE = {
 	email: '',
 	password: '',
-	error: null,
+	formErrors: {email: '', password: ''},
+	emailValid: false,
+	passwordValid: false,
+	formValid: false,
+	error: null
 };
 
 class SignInFormBase extends Component {
@@ -33,33 +40,75 @@ class SignInFormBase extends Component {
 			.then(() => {
 				this.setState({ ...INITIAL_STATE });
 				this.props.history.push(ROUTES.HOME);
+				store.addNotification({
+					title: 'Signin success',
+					message: 'Welcome back',
+					type: 'success',                         // 'default', 'success', 'info', 'warning'
+					container: 'bottom-right',                // where to position the notifications
+					animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied
+					animationOut: ["animated", "fadeOut"],   // animate.css classes that's applied
+					dismiss: {
+						duration: 3000
+					}
+				})
+				console.log("ok")
 			})
 			.catch(error => {
+				console.log("ko")
 				this.setState({ error });
 			});
-
 		event.preventDefault();
 	};
 
 	onChange = event => {
-		this.setState({ [event.target.name]: event.target.value });
+		const name = event.target.name;
+		const value = event.target.value;
+		this.setState({ [event.target.name]: event.target.value },
+			() => { this.validateField(name, value) });
 	};
+
+	validateField(fieldName, value) {
+		let fieldValidationErrors = this.state.formErrors;
+		let emailValid = this.state.emailValid;
+		let passwordValid = this.state.passwordValid;
+
+		switch(fieldName) {
+			case 'email':
+				emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+				fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+				break;
+			case 'password':
+				passwordValid = value.length >= 6;
+				fieldValidationErrors.password = passwordValid ? '': ' must be superior to 6 characters';
+				break;
+			default:
+				break;
+		}
+		this.setState({formErrors: fieldValidationErrors,
+			emailValid: emailValid,
+			passwordValid: passwordValid
+		}, this.validateForm);
+	}
+
+	validateForm() {
+		this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+	}
 
 	render() {
 		const { email, password, error } = this.state;
 
 
 		return (
-			<div className="container">
+			<div class="container">
 				<div className="row">
 					<div className="col-sm-7 col-sm-push-5 col-md-8 col-md-push-4">
 						<div className="text-desclaimer">
 							<h1>Welcome</h1>
 							<h3>to Trip</h3>
 							.<br />
-							<div className="bs-callout callout-info">
+							<div class="bs-callout callout-info">
 								<h4>Présentation</h4>
-								<ul className="text">
+								<ul class="text">
 									<li></li>
 									<li></li>
 								</ul>
@@ -71,18 +120,31 @@ class SignInFormBase extends Component {
 						<div className="box">
 							<h2 className="title">Login</h2>
 							<form onSubmit={this.onSubmit}>
+								{/*<FormErrors formErrors={this.state.formErrors} />*/}
+
 								<div className="field">
-									<input	name="email" value={email} onChange={this.onChange} className="form-control" type="text" placeholder="Email Address" />
+									<input	name="email" value={email} onChange={this.onChange} class="form-control" type="text" placeholder="Email Address" />
 									<i className="fa fa-user user"></i>
 								</div>
+								{ !this.state.emailValid ? (
+									<small className="color-red">{ "invalid email syntax" }</small>
+								) : (
+									""
+								)}
+
 								<div className="field">
-									<input 	name="password" value={password} onChange={this.onChange} className="form-control"  type="password" placeholder="Password" />
+									<input 	name="password" value={password} onChange={this.onChange} class="form-control"  type="password" placeholder="Password" />
 									<i className="fa fa-ellipsis-h"></i>
 								</div>
-								
+								{ !this.state.passwordValid ? (
+									<small className="color-red">{ "password must have more than 6 character" }</small>
+								) : (
+									""
+								)}
+
 								<div className="field footer-form text-right">
-									<span className="remember"><input className="labelauty" type="checkbox" data-labelauty="Keep me signed in" /></span>
-									<button className="btn btn-default button-form"  type="submit">Login</button>
+									<span className="remember"><input class="labelauty" type="checkbox" data-labelauty="Keep me signed in" checked/></span>
+									<button className="btn btn-default button-form"  type="submit" disabled={!this.state.formValid}>Login</button>
 								</div>		
 								{error && <p>{error.message}</p>}
 							</form>		
--- src/component/Sign/SignUp/index.js
+++ src/component/Sign/SignUp/index.js
@@ -5,6 +5,7 @@ import { PasswordForgetLink } from '../../Password/PasswordForget';
 
 import { withBackaccessContext } from '../../BackEnd';
 import * as ROUTES from '../../../const/routes';
+import {store} from "react-notifications-component";
 
 const SignUpPage = () => (
     <section id="user-profile">    
@@ -13,13 +14,20 @@ const SignUpPage = () => (
 );
 
 const INITIAL_STATE = {
-  username: '',
-  name: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
-  isAdmin: false,
-  error: null,
+    username: '',
+    name: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    isAdmin: false,
+    formErrors: {username: '', name: '', email: '', passwordOne: '', passwordTwo: ''},
+    usernameValid: false,
+    nameValid: false,
+    emailValid: false,
+    passwordOneValid: false,
+    passwordTwoValid: false,
+    formValid: false,
+    error: null
 };
 
 const ERROR_CODE_ACCOUNT_EXISTS = 'le compte existe deja';
@@ -39,7 +47,18 @@ class SignUpFormBase extends Component {
             .doCreateUserWithEmailAndPassword({firstname: username, lastname: name, email : email, password : passwordOne})
             .then(() => {
                 this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
+                this.props.history.push(ROUTES.ACCOUNT);
+                store.addNotification({
+                    title: 'Signup success',
+                    message: 'Welcome aboard',
+                    type: 'default',                         // 'default', 'success', 'info', 'warning'
+                    container: 'bottom-right',                // where to position the notifications
+                    animationIn: ["animated", "fadeIn"],     // animate.css classes that's applied
+                    animationOut: ["animated", "fadeOut"],   // animate.css classes that's applied
+                    dismiss: {
+                        duration: 3000
+                    }
+                })
                 console.log("ok")
             })
             .catch(error => {
@@ -53,8 +72,57 @@ class SignUpFormBase extends Component {
   };
 
   onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+      const name = event.target.name;
+      const value = event.target.value;
+      this.setState({ [event.target.name]: event.target.value },
+          () => { this.validateField(name, value) });
   };
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let usernameValid = this.state.usernameValid;
+        let nameValid = this.state.nameValid;
+        let emailValid = this.state.emailValid;
+        let passwordOneValid = this.state.passwordOneValid;
+        let passwordTwoValid = this.state.passwordTwoValid;
+
+
+        switch(fieldName) {
+            case 'username':
+                usernameValid = value.length >= 0;
+                fieldValidationErrors.username = usernameValid ? '': ' must be superior to 6 characters';
+                break;
+            case 'name':
+                nameValid = value.length >= 0;
+                fieldValidationErrors.name = nameValid ? '': ' must be superior to 6 characters';
+                break;
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+                break;
+            case 'passwordOne':
+                passwordOneValid = value.length >= 6;
+                fieldValidationErrors.passwordOne = passwordOneValid ? '': ' must be superior to 6 characters';
+                break;
+            case 'passwordTwo':
+                passwordTwoValid = value.length >= 6;
+                fieldValidationErrors.passwordTwo = passwordTwoValid ? '': ' must be superior to 6 characters';
+                break;
+            default:
+                break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+            usernameValid: usernameValid,
+            nameValid: nameValid,
+            emailValid: emailValid,
+            passwordOneValid: passwordOneValid,
+            passwordTwoValid: passwordTwoValid
+        }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({formValid: this.state.usernameValid && this.state.nameValid && this.state.emailValid && this.state.passwordOneValid && this.state.passwordTwoValid});
+    }
 
   onChangeCheckbox = event => {
         this.setState({ [event.target.name]: event.target.checked });
@@ -84,25 +152,50 @@ class SignUpFormBase extends Component {
                                     <span>User Name</span>
                                     <input  placeholder="Full Name"  value={username} onChange={this.onChange} type="text" class="form-control" name="username" id="username" />
                                 </li>
+                                { !this.state.usernameValid ? (
+                                    <small className="color-red">{ "cant be blank" }</small>
+                                ) : (
+                                    ""
+                                )}
                                 <li>
                                     <span>Name</span>
                                     <input value={name}  onChange={this.onChange} type="text"  placeholder=" Name" class="form-control" name="name" id="name" />
                                 </li>
+                                { !this.state.nameValid ? (
+                                    <small className="color-red">{ "cant be blank" }</small>
+                                ) : (
+                                    ""
+                                )}
                                 <li>
                                     <span>Email Address</span>
                                     <input type="text"  value={email}  onChange={this.onChange} placeholder="Email Address"  class="form-control" name="email" id="email" />
                                 </li>
+                                { !this.state.emailValid ? (
+                                    <small className="color-red">{ "invalid email syntax" }</small>
+                                ) : (
+                                    ""
+                                )}
                                 <li>
                                     <span>Password</span>
                                     <input type="password" class="form-control" value={passwordOne} onChange={this.onChange} name="passwordOne" id="passwordOne" />
                                 </li>
+                                { !this.state.passwordOneValid ? (
+                                    <small className="color-red">{ "password must have more than 6 character" }</small>
+                                ) : (
+                                    ""
+                                )}
                                 <li>
                                     <span>Pepeat Password</span>
                                     <input type="password" class="form-control" value={passwordTwo} onChange={this.onChange} name="passwordTwo" id="passwordTwo" />
                                 </li>
+                                { !this.state.passwordTwoValid ? (
+                                    <small className="color-red">{ "password must have more than 6 character" }</small>
+                                ) : (
+                                    ""
+                                )}
                             </ul>
-                            <div class="col-sm-12 col-md-6 text-right">
-                                <button class="btn btn-default signin-button" type="submit"><i class="fa fa-sign-in"></i> Sign up</button>
+                            <div class="text-right">
+                                <button class="btn btn-default signin-button" type="submit" disabled={!this.state.formValid}><i class="fa fa-sign-in"></i> Sign up</button>
                                 {error && <p>{error.message}</p>}
                             </div>
                         </form>
